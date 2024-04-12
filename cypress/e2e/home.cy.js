71444edc233557def607a8ea0d74d78527804b0e
Caue Almeida <caue.almeida@telefonicabigdata.onmicrosoft.com>
2024-04-12 11:51:15 -0300

    test(e2e): Verifies if text content exists

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
+describe('Home spec', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
   it('passes', () => {
     cy.visit('http://localhost:3000')
+  })
+
+  it('should contain correct Advantages text', () => {
+    cy.get('[data-cy="title-advantages"]').contains('Vantagens do nosso banco:')
   })
 })
