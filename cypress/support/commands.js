1cc08b592c56ed07c56152e9d9199732b348c0ea
Caue Almeida <caue.almeida@telefonicabigdata.onmicrosoft.com>
2024-04-16 10:27:59 -0300

    test(e2e): Verifies if selector contains text

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,7 @@
 Cypress.Commands.add('getByDataCy', (selector) => {
   return cy.get(`[data-cy=${selector}]`);
 });
+
+Cypress.Commands.add('containsText', (selector, text) => {
+  return cy.get(`[data-cy=${selector}]`).contains(text);
+});
