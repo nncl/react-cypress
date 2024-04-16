5ee2e4fa46eab7f925672d882934834a10a2c42f
Caue Almeida <caue.almeida@telefonicabigdata.onmicrosoft.com>
2024-04-16 10:22:08 -0300

    test(e2e): Creates custom command

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,13 +1,9 @@
-describe('Home spec', () => {
+describe('HomePage spec', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
 
-  it('passes', () => {
-    cy.visit('http://localhost:3000')
-  })
-
   it('should contain correct Advantages text', () => {
-    cy.get('[data-cy="title-advantages"]').contains('Vantagens do nosso banco:')
+    cy.getByDataCy('title-advantages').contains('Vantagens do nosso banco:')
   })
 })
