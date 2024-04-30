2eb0c195fc85affaf9a9c5f9df858707c587bb98
Caue Almeida <caue.almeida@telefonicabigdata.onmicrosoft.com>
2024-04-30 13:55:06 -0300

    test(e2e): Validates button depending on device

--- cypress/e2e/mobile-devices.cy.js
+++ cypress/e2e/mobile-devices.cy.js
@@ -20,4 +20,38 @@ describe('MobileDevices spec', () => {
 
     cy.location('pathname').should('eq', '/home/investimentos')
   })
+
+  context('iPhone XR', () => {
+    beforeEach(() => {
+      cy.viewport('iphone-xr')
+    })
+
+    it('should navigate on mobile to Investments Page', () => {
+      // Login
+      cy.getByDataCy('botao-login').click();
+      cy.getByDataCy('email-input').type('hello2@world.com');
+      cy.getByDataCy('senha-input').type('123456');
+      cy.getByDataCy('botao-enviar').click();
+
+      // Hamburguer menu opening
+      cy.getByDataCy('menu-burguer').should('be.visible');
+    })
+  })
+
+  context('Macbook 13', () => {
+    beforeEach(() => {
+      cy.viewport('macbook-13')
+    })
+
+    it('should navigate on mobile to Investments Page', () => {
+      // Login
+      cy.getByDataCy('botao-login').click();
+      cy.getByDataCy('email-input').type('hello2@world.com');
+      cy.getByDataCy('senha-input').type('123456');
+      cy.getByDataCy('botao-enviar').click();
+
+      // Hamburguer menu opening
+      cy.getByDataCy('menu-burguer').should('not.be.visible');
+    })
+  })
 })
