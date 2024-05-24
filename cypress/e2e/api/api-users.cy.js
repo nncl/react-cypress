b63c338f22b145730cdebc517d1b1406ec77eb5a
Caue Almeida <caue.almeida@telefonicabigdata.onmicrosoft.com>
2024-05-24 12:00:56 -0300

    test(e2e): Tests API error

--- cypress/e2e/api/api-users.cy.js
+++ cypress/e2e/api/api-users.cy.js
@@ -1,10 +1,37 @@
 describe('API requests', () => {
   context('GET /users', () => {
+
     it('should return a list of users', () => {
       cy.request('GET', 'http://localhost:8000/users').then((response) => {
         expect(response.status).to.eq(200);
         expect(response.body).to.have.length.greaterThan(1);
       });
     });
+
   });
-})
+
+  context('GET /users/:userId', () => {
+
+    it('should return a single user', () => {
+      cy.request({
+        method: 'GET',
+        url: 'http://localhost:8000/users/1'
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('nome');
+      });
+    });
+
+    it('should return an error when user is invalid', () => {
+      cy.request({
+        method: 'GET',
+        url: 'http://localhost:8000/users/2',
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(404);
+        expect(response.body).to.equal('Not Found');
+      });
+    });
+
+  });
+});
